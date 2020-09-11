8d07d6da5393ae9fcebd3dc9989975a0000c1f03
Malkov Dmitry <fly-agaric@bk.ru>
2020-09-12 00:48:05 +0700

    Added score blok, sound file and Accident logic

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
 'use strict'
 
-const score = document.querySelector('.score'),
+const MAX_ENEMY = 8
+
+const currentScore = document.querySelector('.current-score'),
 	start = document.querySelector('.start'),
 	gameArea = document.querySelector('.gameArea'),
-	car = document.createElement('div')
+	car = document.createElement('div'),
+	youScoreBox = document.querySelector('.you-score-box')
+console.log(start)
+
+const audioMainTheme = document.createElement('embed') // embed - добавляем видео + аудио
+// const audioMainTheme = document.createElement('audio')
+
+audioMainTheme.src = '../sound/main_theme.mp3'
+audioMainTheme.type = 'audio/mpeg'
+audioMainTheme.style.cssText = `position: absolute; top: 0px;`
 
 car.classList.add('car')
 
 start.addEventListener('click', startGame)
 document.addEventListener('keydown', startRun)
 document.addEventListener('keyup', stopRun)
+
+const SETTING = {
+	start: false,
+	currentScore: 0,
+	speed: 9,
+	dodge: 5,
+	traffic: 3
+}
 
 const keys = {
 	ArrowUp: false,
@@ -18,84 +37,93 @@ const keys = {
 	ArrowLeft: false
 }
 
-const setting = {
-	start: false,
-	score: 0,
-	speed: 3,
-	traffic: 3
-}
-
 function getQuantityElements(heightElement) { //функция вычисляет, сколько объектов определенной высоты войдет на экран.
 	return document.documentElement.clientHeight / heightElement + 1 //document.documentElement.clientHeight - Вычисляем высоту страницы
 }
-console.log(getQuantityElements(100));
+console.log(getQuantityElements(120));
 
 function startGame() {
 	start.classList.add('hide')
+	gameArea.innerHTML = ''
+	youScoreBox.classList.remove('hide')
+
 
 	for (let i = 0; i < getQuantityElements(100); i++) {
 		const line = document.createElement('div')
 		line.classList.add('line')
 		line.style.top = (i * 100) + 'px'
 		line.y = i * 100
-		gameArea.appendChild(line)
+		gameArea.append(line)
 	}
 
-	for (let i = 0; i < getQuantityElements(157 * setting.traffic); i++) {
+	for (let i = 0; i < getQuantityElements(157 * SETTING.traffic); i++) {
 		const enemy = document.createElement('div')
+		const randomEnemy = Math.floor(Math.random() * MAX_ENEMY) + 1
+		console.log('randomEnemy: ', randomEnemy);
 		enemy.classList.add('enemy')
-		enemy.y = -157 * setting.traffic * (i + 1)
-		enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 52.5)) + 'px'
+		enemy.y = -120 * SETTING.traffic * (i + 1)
+		enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px'
 		enemy.style.top = enemy.y + 'px'
-		// enemy.style.background = 'transparent url("../img/enemy.png") center / cover no-repeat'
-		gameArea.appendChild(enemy)
+		enemy.style.background = `transparent url(../img/enemy${randomEnemy}.png) center / cover no-repeat`
+		gameArea.append(enemy)
 	}
 
-	setting.start = true
-	gameArea.appendChild(car)
-	setting.x = car.offsetLeft //offsetLeft - начальное значение автомобиля - берется из стиля css, класса .car
-	setting.y = car.offsetTop //offsetTop - начальное значение автомобиля - берется из стиля css, класса .car
+	SETTING.currentScore = 0
+	SETTING.start = true
+	gameArea.append(car)
+	car.style.left = (gameArea.offsetWidth - car.offsetWidth) / 2;
+	car.style.top = "auto";
+	car.style.bottom = "10px";
+	// document.body.append(audioMainTheme) //Добавляем видео и аудио на страницу
+	SETTING.x = car.offsetLeft //offsetLeft - начальное значение автомобиля - берется из стиля css, класса .car
+	SETTING.y = car.offsetTop //offsetTop - начальное значение автомобиля - берется из стиля css, класса .car
 	requestAnimationFrame(playGame)
 }
 
 function playGame() {
-	if (setting.start) {
+	if (SETTING.start) {
+		currentScore.innerHTML = `${SETTING.currentScore}`
+
 		moveRoad()
 		moveEnemy()
 
-		if (keys.ArrowLeft && setting.x > 0) {
-			setting.x -= setting.speed
+		if (keys.ArrowLeft && SETTING.x > 0) {
+			SETTING.x -= SETTING.dodge
 		}
-		if (keys.ArrowRight && setting.x < gameArea.offsetWidth - car.offsetWidth) {
-			setting.x += setting.speed
+		if (keys.ArrowRight && SETTING.x < gameArea.offsetWidth - car.offsetWidth) {
+			SETTING.x += SETTING.dodge
 		}
-		if (keys.ArrowUp && setting.y > 0) {
-			setting.y -= setting.speed
+		if (keys.ArrowUp && SETTING.y > 0) {
+			SETTING.y -= SETTING.speed
 		}
-		if (keys.ArrowDown && setting.y < gameArea.offsetHeight - car.offsetHeight) {
-			setting.y += setting.speed
+		if (keys.ArrowDown && SETTING.y < gameArea.offsetHeight - car.offsetHeight) {
+			SETTING.y += SETTING.speed
 		}
 
-		car.style.left = setting.x + 'px'
-		car.style.top = setting.y + 'px'
+		car.style.left = SETTING.x + 'px'
+		car.style.top = SETTING.y + 'px'
 		requestAnimationFrame(playGame) // Recursion - the function starts itself
 	}
 }
 
 function startRun(event) {
-	event.preventDefault()
-	keys[event.key] = true
+	if (keys.hasOwnProperty(event.key)) {
+		event.preventDefault()
+		keys[event.key] = true
+	}
 }
 
 function stopRun(event) {
-	event.preventDefault()
-	keys[event.key] = false
+	if (keys.hasOwnProperty(event.key)) {
+		event.preventDefault()
+		keys[event.key] = false
+	}
 }
 
 function moveRoad() {
 	let lines = document.querySelectorAll('.line')
 	lines.forEach(function(line) {
-		line.y += setting.speed
+		line.y += SETTING.speed
 		line.style.top = line.y + 'px'
 
 		if (line.y >= document.documentElement.clientHeight) { // document.documentElement.clientHeight - Получаем высоту страницы
@@ -107,12 +135,37 @@ function moveRoad() {
 function moveEnemy() {
 	let enemy = document.querySelectorAll('.enemy')
 	enemy.forEach(function(item) {
-		item.y += setting.speed / 1.5
+		let carRect = car.getBoundingClientRect() // getBoundingClientRect - определяем координаты объекта (car - игрок) top, bottom, left, right x, y
+		let enemyRect = item.getBoundingClientRect() // getBoundingClientRect - определяем координаты объекта(item - enemy) top, bottom, left, right x, y
+
+		if (carRect.top <= enemyRect.bottom &&
+			carRect.right >= enemyRect.left &&
+			carRect.left <= enemyRect.right &&
+			carRect.bottom >= enemyRect.top) {
+			SETTING.start = false
+			console.warn('ДТП')
+			start.classList.remove('hide')
+
+		}
+
+		item.y += SETTING.speed / 1.5
 		item.style.top = item.y + 'px'
 
 		if (item.y >= document.documentElement.clientHeight) { // document.documentElement.clientHeight - Получаем высоту страницы
-			item.y = -157 * setting.traffic
+			item.y = -157 * SETTING.traffic
 			item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 52.5)) + 'px'
+			let newRandomEnemy = Math.floor(Math.random() * MAX_ENEMY) + 1
+
+			if (SETTING.speed == 9) {
+				SETTING.currentScore += 1
+			}
+			if (SETTING.speed == 12) {
+				SETTING.currentScore += 2
+			}
+			if (SETTING.speed == 15) {
+				SETTING.currentScore += 3
+			}
+			item.style.background = `transparent url(../img/enemy${newRandomEnemy}.png) center / cover no-repeat`
 		}
 	})
 }
