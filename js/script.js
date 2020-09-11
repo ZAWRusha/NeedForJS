1042253b21963ca2fce5bda6052ac9aa6732ef42
Malkov Dmitry <fly-agaric@bk.ru>
2020-09-12 01:00:25 +0700

    fix url enemy in script.js

--- js/script.js
+++ js/script.js
@@ -64,7 +64,7 @@ function startGame() {
 		enemy.y = -120 * SETTING.traffic * (i + 1)
 		enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px'
 		enemy.style.top = enemy.y + 'px'
-		enemy.style.background = `transparent url(../img/enemy${randomEnemy}.png) center / cover no-repeat`
+		enemy.style.background = `transparent url("../img/enemy${randomEnemy}.png") center / cover no-repeat`
 		gameArea.append(enemy)
 	}
 
